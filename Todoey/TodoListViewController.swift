00cd7d61c155ffa0127b91b38a0f8ec7d1776cd7
Mseak <ivan.gr.mseak@gmail.com>
2019-08-19 14:59:24 -0500

    Código debidamente comentado y avanzado hasta la parte de enviar una alerta.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    
+    /*
+    Definimos a variable de tipo arreglo con el cual introduciremos elementos a la tableview
+    */
     let itemArray = ["Find Mike", "Buy eggos", "Destroy Demogorgon"]
     
 
@@ -21,11 +23,16 @@ class TodoListViewController: UITableViewController {
 
     //Mark - Table View Methods
     
+    /*
+     Este metodo contiene el codigo necesario para recuperar el contenido de la tableView y obtener el siguiente numero de la lista de Items
+    */
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
-    
+    /*
+    Con este metodo obtenemos el texto del item deseado de la lista de objetos en el TableView
+    */
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
@@ -36,14 +43,22 @@ class TodoListViewController: UITableViewController {
         
     }
     
-    //MARK -Table View Delegates
-    
+    /*MARK -Table View Delegates
+    /Este metodo crea un manejador para hacer una accion cuando un item de la lista del table view sea seleccionado, en este caso
+    /se colocara una marca de check cada vez que el item sea tocado
+    /
+    /se declara el metodo a continuacion con el parametro de didSelectRow
+    */
+ 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        //let selectedTrail = trails[indexPath.row]
+        /*
+         Esta linea quita la seleccion del item tocado de manera animada para que la interfaz tenga mas estetica
+        */
         tableView.deselectRow(at: indexPath, animated: true)
-        
-        
+        /*
+         El siguiente if es el que no permite revisar si el item de la lista tiene ya un checkmark al ser tocado y si lo tiene,
+         Quitar el checkmark. si no lo tiene al momento de ser tocado le coloca el checkmark
+        */
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
             
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
@@ -51,12 +66,36 @@ class TodoListViewController: UITableViewController {
             
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-        
+        /*
+        Con esta linea imprimimos en consola el contenido de la variable local indexPath para comprobar que se entra dentro de este metodo y todo
+        esta bien
+        */
         print(itemArray[indexPath.row])
         
     
     }
     
+    
+    //MARK - Add New Items Section
+    //Esta funcion hace el mapeo del signo de mas en la interfaz de la app
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        //Esta instruccion crea una variable de tipo UIAlertController con titulo mensaje y estilo
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        //Esta instruccion me crea una accion que el programa hara cuando mi boton de + se presione y que añadira un nuevo item a mi col
+        //de todoey
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            //What will happen once the user clicks the add item button on our UIAlert
+            print("Success!")
+        }
+        //creamos el lamado a la accion
+        alert.addAction(action)
+        //crea la animacion del mensaje en donde el usuario escribira el titulo del nuevo item de la lista de TO DO
+        present(alert ,animated: true, completion: nil)
+        
+        
+    }
     
     
 }
